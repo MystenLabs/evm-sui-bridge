c30e115cf722cce34c18e0173120c79555cf267b
bridgerz <bridgerzoske@gmail.com>
2023-12-18 09:55:23 -0800

    Init 🚀

---
+++ hardhat.config.ts
@@ -0,0 +1,107 @@
+const fs = require("fs");
+import { HardhatUserConfig, task } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types";
+import { config as dotenvConfig } from "dotenv";
+import { resolve } from "path";
+import "hardhat-dependency-compiler";
+import "hardhat-preprocessor";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import "@nomicfoundation/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomicfoundation/hardhat-verify";
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+const chainIds = {
+  mainnet: 1,
+  goerli: 5,
+  hardhat: 1337,
+};
+
+const MNEMONIC = process.env.MNEMONIC || "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+
+function createTestnetConfig(
+  network: keyof typeof chainIds
+): NetworkUserConfig {
+  const url: string = "https://" + network + ".infura.io/v3/" + INFURA_API_KEY;
+  return {
+    accounts: {
+      count: 10,
+      initialIndex: 0,
+      mnemonic: MNEMONIC,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[network],
+    url,
+    saveDeployments: true,
+  };
+}
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+      chainId: chainIds.hardhat,
+      saveDeployments: true,
+    },
+    localhost: {
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+      chainId: chainIds.hardhat,
+      saveDeployments: true,
+    },
+    mainnet: createTestnetConfig("mainnet"),
+    goerli: createTestnetConfig("goerli"),
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.15",
+      },
+    ],
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+    deployments: "./deployments",
+    deploy: "./deploy",
+    imports: "./artifacts",
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match(find)) {
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+export default config;
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
---
+++ utils/utils.ts
@@ -0,0 +1,75 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeploymentSubmission } from "hardhat-deploy/dist/types"
+import { DeployProxyOptions } from "@openzeppelin/hardhat-upgrades/dist/utils/options"
+import { ethers } from "ethers"
+
+export const deployProxyAndSave = async (
+  name: string,
+  args: any,
+  hardhat: HardhatRuntimeEnvironment,
+  deployOptions?: DeployProxyOptions
+): Promise<string> => {
+  return await deployProxyAndSaveAs(name, name, args, hardhat, deployOptions)
+}
+
+export const deployProxyAndSaveAs = async (
+  factoryName: string,
+  name: string,
+  args: any,
+  hardhat: HardhatRuntimeEnvironment,
+  deployOptions?: DeployProxyOptions
+): Promise<string> => {
+  const contractFactory = await hardhat.ethers.getContractFactory(factoryName)
+  let deployment = await hardhat.deployments.getOrNull(name)
+
+  if (deployment) {
+    console.log("✅ ", name, " already deployed")
+    return deployment.address
+  }
+
+  let abi = (await hardhat.artifacts.readArtifact(name)).abi
+
+  let contract = await hardhat.upgrades.deployProxy(contractFactory, args, deployOptions)
+
+  contract = await contract.waitForDeployment()
+
+  let receipt = await contract.deploymentTransaction()
+  let tx = await receipt?.getTransaction()
+  let proxyAddress = await contract.getAddress()
+  const implAddress = await hardhat.upgrades.erc1967.getImplementationAddress(proxyAddress)
+  if (!receipt || !tx || !proxyAddress) return ""
+
+  const contractDeployment = {
+    address: proxyAddress,
+    abi,
+    receipt: {
+      from: receipt.from,
+      transactionHash: receipt.hash,
+      blockHash: receipt.blockHash,
+      blockNumber: receipt.blockNumber,
+      transactionIndex: tx.index,
+      cumulativeGasUsed: 0,
+      gasUsed: 0,
+    },
+    metadata: "implementationAddress: " + implAddress,
+  } as DeploymentSubmission
+
+  await hardhat.deployments.save(name, contractDeployment)
+
+  console.log("🚀 ", name, " deployed at ", proxyAddress)
+  return proxyAddress
+}
+
+export const formatStableCredits = (value: ethers.BigNumberish) => {
+  return ethers.formatUnits(value, "mwei")
+}
+
+export const parseStableCredits = (value: string) => {
+  return ethers.parseUnits(value, "mwei")
+}
+
+export const getConfig = () => {
+  let adminOwner = process.env.ADMIN_OWNER_ADDRESS
+  let reserveTokenAddress = process.env.RESERVE_TOKEN_ADDRESS
+  return { adminOwner, reserveTokenAddress }
+}
