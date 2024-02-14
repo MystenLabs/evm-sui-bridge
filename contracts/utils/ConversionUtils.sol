06c075ea84dce9ae60db7b2301f6f230c1ce0fda
George Digkas <george.digkas@mystenlabs.com>
2024-02-14 11:54:14 +0200

    update docs

--- contracts/utils/ConversionUtils.sol
+++ contracts/utils/ConversionUtils.sol
@@ -5,10 +5,25 @@ pragma solidity ^0.8.20;
 /// @notice A library to provide utility functions
 /// @dev This library is used to provide utility functions
 library ConversionUtils {
-    // Define a function to calculate the converted amount based on the decimals
+    
+    /// @notice Ensure the decimals are valid
+    /// @param ethDecimal The decimal of the ETH token
+    /// @param suiDecimal The decimal of the SUI token
+    modifier validDecimals(uint8 ethDecimal, uint8 suiDecimal) {
+        require(ethDecimal <= 19, "ConversionUtils: Invalid ETH decimal");
+        require(suiDecimal <= 19, "ConversionUtils: Invalid SUI decimal");
+        _;
+    }
+
+    /// @notice Calculate the converted amount from ETH to SUI decimal
+    /// @param ethDecimal The decimal of the ETH token
+    /// @param suiDecimal The decimal of the SUI token
+    /// @param amount The amount to convert
+    /// @return The converted amount
     function calculateEthToSuiConvertedAmount(uint8 ethDecimal, uint8 suiDecimal, uint256 amount)
         internal
         pure
+        validDecimals(ethDecimal, suiDecimal)
         returns (uint256)
     {
         if (ethDecimal == suiDecimal) {
@@ -24,10 +39,15 @@ library ConversionUtils {
         }
     }
 
-    // Define a function to calculate the converted amount based on the decimals
+    /// @notice Calculate the converted amount from SUI to ETH decimal
+    /// @param ethDecimal The decimal of the ETH token
+    /// @param suiDecimal The decimal of the SUI token
+    /// @param amount The amount to convert
+    /// @return The converted amount
     function calculateSuiToEthConvertedAmount(uint8 ethDecimal, uint8 suiDecimal, uint256 amount)
         internal
         pure
+        validDecimals(ethDecimal, suiDecimal)
         returns (uint256)
     {
         if (suiDecimal == ethDecimal) {
